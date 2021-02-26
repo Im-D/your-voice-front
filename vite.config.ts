4fdcfaa2c45fb9fc3aeaabc891f05c97d32af036
YongjaeLee <dev.yongjaelee@gmail.com>
2021-02-26 21:51:22 +0900

    fix: vite.config.ts의 @atoms alias 설정 depth 수정
    - changelog.config.js 삭제 및 vite.config.ts 추가
    - 이전 커밋은 잘못된 커밋

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     '@': path.resolve(__dirname, './src'),
     '@assets': path.resolve(__dirname, './src/assets'),
     '@components': path.resolve(__dirname, './src/components'),
-    '@atoms': path.resolve(__dirname, './src/components/atoms'),
+    '@atoms': path.resolve(__dirname, './components/atoms'),
     '@molecules': path.resolve(__dirname, './src/components/molecules'),
     '@organisms': path.resolve(__dirname, './src/components/organisms'),
     '@pages': path.resolve(__dirname, './src/pages'),
