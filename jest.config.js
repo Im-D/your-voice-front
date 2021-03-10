8f452ee49ddb3e01c8e824f10f62fec21f0c3378
YongjaeLee <dev.yongjaelee@gmail.com>
2021-03-11 01:28:41 +0900

    style: jest.config.js 컨벤션 통일
    - 싱글쿼트와 더블쿼트 혼용된 부분 수정
    - prettier로 정리

--- jest.config.js
+++ jest.config.js
@@ -12,13 +12,13 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
       'identity-obj-proxy',
     '^@assets(.*)&': '<rootDir>/src/assets$1',
-    "^@components(.*)$": "<rootDir>/src/components$1",
-    '^@atoms(.*)$':  '<rootDir>/src/atoms$1',
-    '^@molecules(.*)$':  '<rootDir>/src/components/molecules$1',
-    '^@organisms(.*)$':  '<rootDir>/src/components/organisms$1',
-    '^@pages(.*)$':  '<rootDir>/src/pages$1',
-    '^@store(.*)$':  '<rootDir>/src/store$1',
-    '^@utils(.*)$':  '<rootDir>/src/utils$1',
-    '^@api(.*)$':  '<rootDir>/src/api$1',
-  }
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@atoms(.*)$': '<rootDir>/src/atoms$1',
+    '^@molecules(.*)$': '<rootDir>/src/components/molecules$1',
+    '^@organisms(.*)$': '<rootDir>/src/components/organisms$1',
+    '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@store(.*)$': '<rootDir>/src/store$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1',
+    '^@api(.*)$': '<rootDir>/src/api$1',
+  },
 };
