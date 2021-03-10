fb0f55a964dd8d59326ebb8f182a92929be276b2
SeonHyungJo <seonhyung.jo@gmail.com>
2021-03-11 01:14:19 +0900

    feat(logo): logo 컴포넌트 개발
    - className, width, height props 추가
    
    re #18

--- src/atoms/Logo/index.tsx
+++ src/atoms/Logo/index.tsx
@@ -1,5 +1,27 @@
 import React, { ReactElement } from 'react';
 
-export default function Logo(): ReactElement {
-  return <></>;
+import Logo from '@assets/Logo.png';
+
+import './style.scss';
+
+interface LogoProps {
+  className: string;
+  width: number;
+  height: number;
+}
+
+export default function Logo({
+  className,
+  width = 60,
+  height = 60,
+}: LogoProps): ReactElement {
+  return (
+    <img
+      className={`your-voice-emoji ${className}`}
+      src={Logo}
+      alt="logo"
+      width={width}
+      height={height}
+    />
+  );
 }
