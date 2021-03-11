b0e74f47a05fa1993a11ca450172a97c75179376
SeonHyungJo <seonhyung.jo@gmail.com>
2021-03-12 00:33:16 +0900

    refactor(logo): logo className 기본값 설정 및 props optional로 전환

--- src/atoms/Logo/index.tsx
+++ src/atoms/Logo/index.tsx
@@ -5,13 +5,13 @@ import Logo from '@assets/Logo.png';
 import './style.scss';
 
 interface LogoProps {
-  className: string;
-  width: number;
-  height: number;
+  className?: string;
+  width?: number;
+  height?: number;
 }
 
 export default function Logo({
-  className,
+  className = '',
   width = 60,
   height = 60,
 }: LogoProps): ReactElement {
